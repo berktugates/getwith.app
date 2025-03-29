a8485adc474d87e20c14f10aaa35c3f383f91317
Berktug Berke Ates <72451462+berktugates@users.noreply.github.com>
2025-03-29 06:44:46 +0300

    reset

--- app/index.tsx
+++
@@ -1,8 +0,0 @@
-import { Redirect } from 'expo-router';
-import SignIn from './auth/SignIn/SignIn';
-
-
-// user login oldugunda ise kosullu olarak home page'e yonlendir.
-export default function Index() {
-  return <Redirect href="/auth/SignIn/SignIn" />; 
-}

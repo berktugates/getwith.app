91e412ee6cbe9ff5f5271ae5fad62284ba47622a
Berktug Berke Ates <72451462+berktugates@users.noreply.github.com>
2025-04-02 18:30:32 +0300

    fix:status bar

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -5,48 +5,58 @@ import { Button } from "@/components/ui/button";
 import { Categories } from "@/constants/Categories";
 import AdsCard from "../../components/explore/AdsCard";
 import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
 
 export default function TabTwoScreen() {
   const [isPressedCategory, setIsPressedCategory] = useState<number>();
   return (
-    <ScrollView bounces={false} showsVerticalScrollIndicator={false}>
-      <SafeAreaView>
-        <View id="root" className="">
-          <Header />
-          <View id="recommended" className="p-6">
-            <View className="flex flex-row items-center justify-between">
-              <Text className="text-2xl font-semibold">
-                Recommended For You
-              </Text>
-              <Button className="bg-stone-300">
-                <Text>See All</Text>
-              </Button>
-            </View>
-            <View id="categories" className="mt-4">
-              <FlatList
-                horizontal
-                showsHorizontalScrollIndicator={false}
-                data={Categories}
-                keyExtractor={(item) => item.id.toString()}
-                renderItem={(item) => (
-                  <Category
-                    onPress={() => setIsPressedCategory(item.item.id)}
-                    item={item.item}
-                    selectedCategory={isPressedCategory}
-                  />
-                )}
+    <>
+      <StatusBar style="light" backgroundColor="black" />
+      <SafeAreaView className="bg-black">
+        <View id="root" className="h-screen bg-black">
+          <ScrollView
+            bounces={false}
+            showsVerticalScrollIndicator={false}
+            className="bg-white"
+          >
+            <Header />
+            <View id="recommended" className="p-6">
+              <View className="flex flex-row items-center justify-between">
+                <Text className="text-2xl font-semibold">
+                  Recommended For You
+                </Text>
+                <Button className="bg-stone-300">
+                  <Text>See All</Text>
+                </Button>
+              </View>
+              <View id="categories" className="mt-4">
+                <FlatList
+                  horizontal
+                  showsHorizontalScrollIndicator={false}
+                  data={Categories}
+                  keyExtractor={(item) => item.id.toString()}
+                  renderItem={(item) => (
+                    <Category
+                      onPress={() => setIsPressedCategory(item.item.id)}
+                      item={item.item}
+                      selectedCategory={isPressedCategory}
+                    />
+                  )}
+                />
+              </View>
+              <AdsCard
+                company={"X Limited Corp."}
+                jobTitle={"Full Stack Software Engineer"}
+                jobDesc={"Lorem ipsum dolor sit amet."}
+                jobLocation={"Remote"}
+                icon={
+                  "https://upload.wikimedia.org/wikipedia/commons/c/ce/X_logo_2023.svg"
+                }
               />
             </View>
-            <AdsCard
-              company={"X Limited Corp."}
-              jobTitle={"Full Stack Software Engineer"}
-              jobDesc={"Lorem ipsum dolor sit amet."}
-              jobLocation={"Remote"}
-              icon={"https://upload.wikimedia.org/wikipedia/commons/c/ce/X_logo_2023.svg"}
-            />
-          </View>
+          </ScrollView>
         </View>
       </SafeAreaView>
-    </ScrollView>
+    </>
   );
 }
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,10 +1,12 @@
 import { SafeAreaView, View } from "react-native";
+import { StatusBar } from "expo-status-bar";
 import CardSwiper from "@/components/home/CardSwiper";
 import Header from "@/components/home/Header";
 
 export default function HomeScreen() {
   return (
     <SafeAreaView className="h-screen">
+      <StatusBar style="dark" />
       <Header />
       <View id="content">
         <CardSwiper />
